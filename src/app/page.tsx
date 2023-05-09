9193bae72dc93d73a6f9a9518ac7527c2354b946
igkokici <igli.kokici@gmail.com>
2023-05-09 17:44:31 +0200

    feat: services I offer section and starting with portofolio section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,115 @@
+"use client"
+
+import { useState } from 'react';
 import Image from 'next/image'
 import { BsFillMoonStarsFill } from "react-icons/bs"
 import { AiFillLinkedin, AiFillInstagram } from "react-icons/ai"
-import devEd from "../../public/dev-ed-wave.png"
+import devIgli from "../../public/dev-ed-wave.png"
+import code from "../../public/code.png";
+
+type IconType = 'instagram' | 'linkedin'
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const onIconClick = (icon: IconType) => {
+    if(icon === 'linkedin'){
+      window.open("https://www.linkedin.com/in/igli-kokici-7225a2179/")
+    } else {
+      window.open('https://www.instagram.com/')
+    }
+  }
+
   return (
-    <main className=' bg-white px-10'>
-      <section className='min-h-screen'>
-        <nav className=' py-10 mb-12 flex justify-between'>
-          <h1 className=' text-xl font-burtons'>developedbyIgli</h1>
-          <ul className=' flex items-center'>
-            <li>
-              <BsFillMoonStarsFill className=' cursor-pointer text-2xl' />
-            </li>
-            <li>
-              <a className=' bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2  rounded-md ml-8' href='#'>Resume</a>
-            </li>
-          </ul>
-        </nav>
-        <div className='text-center p-10'>
-          <h2 className=' text-5xl py-2 text-teal-600 font-medium'>Igli Kokici</h2>
-          <h3 className=' text-2xl py-2'>Software Developer</h3>
-          <p className=' text-md py-5 leading-8'>
-            Providing freelancing services for programming needs.
-          </p>
-        </div>
-        <div className=' text-5xl flex justify-center gap-16 py-3 text-gray-600'>
-          <AiFillLinkedin className=' cursor-pointer'/>
-          <AiFillInstagram className=' cursor-pointer'/>
-        </div>
-        <div className=' relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
-          <Image src={devEd} alt='' fill style={{ objectFit: 'cover' }}/>
-        </div>
-      </section>
-    </main>
-  )
+    <div className={darkMode ? "dark" : ""}>
+      <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+            <h1 className="font-burtons text-xl">ByIgli</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill
+                  onClick={() => setDarkMode(!darkMode)}
+                  className=" cursor-pointer text-2xl"
+                />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10 py-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
+              Igli Kokici
+            </h2>
+            <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
+              Frontend Developer.
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
+              Software developer providing services for programming needs. Join me down below and let's get cracking!
+            </p>
+            <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+              <AiFillInstagram className=' cursor-pointer' onClick={()=> onIconClick('instagram')}/>
+              <AiFillLinkedin className=' cursor-pointer' onClick={()=> onIconClick('linkedin')}/>
+            </div>
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+              <Image src={devIgli} alt='' layout="fill" objectFit="cover" />
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as a developer, I've done work for
+              <span className="text-teal-500"> agencies </span>
+              and <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              I offer from a wide range of services, including apps and web programming.
+            </p>
+          </div>
+          <div className="lg:flex gap-10">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
+              <Image alt='' src={code} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2 ">
+                Code your dream project
+              </h3>
+              <p className="py-2">
+                Do you have an idea for your next great website? Let's make it a
+                reality.
+              </p>
+              <h4 className="py-4 text-teal-600">Design Tools I am fimiliar with</h4>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Indesign</p>
+            </div>
+          
+          </div>
+        </section>
+        <section className="py-10">
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Since the beginning of my journey as a developer designer, I've done remote work for
+              <span className="text-teal-500"> agencies </span>
+              consulted for <span className="text-teal-500">startups </span>
+              and collaborated with talanted people to create digital products
+              for both business and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              I offer from a wide range of services, including mobile and web programming
+            </p>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
